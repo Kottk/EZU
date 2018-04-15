451708fead1bde191c6ea2ead513e7242ae9a9f5
Sean Kirwan <xeseanh4x@gmail.com>
2018-04-15 19:17:26 -0400

    Update FazeCastTest.java

--- FazeCastTest.java
+++ FazeCastTest.java
@@ -2,13 +2,11 @@ import com.fazecast.jSerialComm.*;
 
 public class FazeCastTest {
     public static void main(String[] args) {
-        SerialPort comPort = SerialPort.getCommPorts()[1];
-        SerialPort comPortL = SerialPort.getCommPorts()[2];
-
+        SerialPort comPort = SerialPort.getCommPorts()[3];
+        
         byte[] message = {(byte)0x80, 0x10, (byte)0xF0, 0x08, (byte)0xA8, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x1C, 0x54};
 
         comPort.setComPortParameters(4800,8,1,0);
-        comPortL.setComPortParameters(4800,8,1,0);
         comPort.openPort();
         comPort.writeBytes(message, message.length);
 
