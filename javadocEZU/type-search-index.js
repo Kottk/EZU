7176e053c1e216fabcd897cb39fce2d72ea4af00
Tony <a.t.kottke@gmail.com>
2018-05-03 09:28:37 -0400

    Removed unneccessary javadoc vomit

---
+++ javadocEZU/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Driver"},{"p":"<Unnamed>","l":"ECUInitializer"},{"p":"<Unnamed>","l":"ECUSensor"},{"p":"<Unnamed>","l":"EngineSpeed"},{"p":"<Unnamed>","l":"VehicleSpeed"}]
